796f59ecc8c4b7c110be80e8cf0c7c1927872ad0
Ahmad <ahmadbabood4@gmail.com>
2022-06-02 11:42:12 +0300

    past main app in it

---
+++ main-app/db.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+//                                  DataName
+var mongoDB = 'mongodb://127.0.0.1/NewDataBase';
+mongoose.connect(mongoDB, 
+  {useNewUrlParser: true, useUnifiedTopology: true}
+  ,()=>{
+    console.log('data base on...');
+  });
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+db.on('connected',()=>{
+  console.log('mongoDB is connected');
+})
